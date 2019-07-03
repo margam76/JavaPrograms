6b0e6bd12572db077f1a63f9e10ad3892c4ede01
margam76 <50553305+margam76@users.noreply.github.com>
2019-07-03 14:39:57 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        int myfirstnumber=5;
+        System.out.println(myfirstnumber);
+
+    }
+}
